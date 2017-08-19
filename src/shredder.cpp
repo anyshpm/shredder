44652b6ad13819d48a177d1cee509715b9146c51
anyshpm <anyshpm@anyshpm.com>
2017-08-19 17:12:30 +0800

    init

--- src/shredder.cpp
+++ src/shredder.cpp
@@ -3,38 +3,60 @@
  Name        : shredder.cpp
  Author      : anyshpm
  Version     :
- Copyright   : 
+ Copyright   :
  Description : Hello World in C++,
  ============================================================================
  */
 
 #include <wx/wx.h>
-#include <wx/progdlg.h>
+#include <wx/iconloc.h>
+#include <wx/taskbar.h>
 
-class Simple: public wxFrame
+class ShredderTaskBarIcon: public wxTaskBarIcon
 {
     public:
-        Simple(const wxString& title );
+        void OnAbout(wxCommandEvent& event)
+        {
+            wxMessageBox(_T("hello!"), wxT("About Tray!"), wxOK | wxICON_INFORMATION);
+        }
+
+    protected:
+        virtual wxMenu *CreatePopupMenu()
+        {
+            // 每次都要 new 一个新的 wxMenu
+            wxMenu *popupMenu = new wxMenu();
+            popupMenu->Append(wxID_ABOUT, _("About"));
+            popupMenu->AppendSeparator();
+            popupMenu->Append(wxID_EXIT, _("Exit"));
+            return popupMenu;
+        };
+
+    private:
+        DECLARE_EVENT_TABLE();
 };
 
-class MyApp : public wxApp
+class Shredder: public wxApp
 {
     public:
-        virtual bool OnInit ();
+        virtual bool OnInit ()
+        {
+            trayIcon = new ShredderTaskBarIcon();
+            trayIcon->SetIcon(wxIcon("icon.bmp"));
+            return true ;
+        }
+        ~Shredder()
+        {
+            if (trayIcon != NULL)
+            {
+                delete trayIcon;
+            }
+        }
+
+    private:
+        ShredderTaskBarIcon* trayIcon;
 };
 
-Simple::Simple (const wxString& title ) :     wxFrame(NULL, wxID_ANY, title, wxDefaultPosition, wxSize( 250, 150))
-{
-    Centre();
-}
-
-IMPLEMENT_APP(MyApp)
-
-bool MyApp::OnInit ()
-{
-    //Simple * simple = new Simple(wxT("Simple")) ;
-    wxProgressDialog* dlg = new wxProgressDialog("title", "message");
-    dlg->Show(true) ;
-    return true ;
-}
-
+BEGIN_EVENT_TABLE(ShredderTaskBarIcon, wxTaskBarIcon)  
+    EVT_MENU(wxID_ABOUT, ShredderTaskBarIcon::OnAbout)  
+END_EVENT_TABLE() 
+IMPLEMENT_APP(Shredder)
