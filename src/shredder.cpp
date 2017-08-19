eeb06721ab4ecc341890b49721d4b3a80c576937
anyshpm <anyshpm@anyshpm.com>
2017-08-19 11:12:39 +0800

    init

---
+++ src/shredder.cpp
@@ -0,0 +1,18 @@
+/*
+ ============================================================================
+ Name        : shredder.cpp
+ Author      : anyshpm
+ Version     :
+ Copyright   : 
+ Description : Hello World in C++,
+ ============================================================================
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+	cout << "Hello World" << endl; /* prints Hello World */
+	return 0;
+}
