2415b4a15de56f16f0776d988a4349dbda0da6f8
anyshpm <anyshpm@anyshpm.com>
2017-08-19 13:24:18 +0800

    init

--- src/shredder.cpp
+++ src/shredder.cpp
@@ -8,11 +8,33 @@
  ============================================================================
  */
 
-#include <iostream>
+#include <wx/wx.h>
+#include <wx/progdlg.h>
 
-using namespace std;
+class Simple: public wxFrame
+{
+    public:
+        Simple(const wxString& title );
+};
 
-int main(void) {
-	cout << "Hello World" << endl; /* prints Hello World */
-	return 0;
+class MyApp : public wxApp
+{
+    public:
+        virtual bool OnInit ();
+};
+
+Simple::Simple (const wxString& title ) :     wxFrame(NULL, wxID_ANY, title, wxDefaultPosition, wxSize( 250, 150))
+{
+    Centre();
 }
+
+IMPLEMENT_APP(MyApp)
+
+bool MyApp::OnInit ()
+{
+    //Simple * simple = new Simple(wxT("Simple")) ;
+    wxProgressDialog* dlg = new wxProgressDialog("title", "message");
+    dlg->Show(true) ;
+    return true ;
+}
+
